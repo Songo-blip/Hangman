884ee16f4fddeefed46b6837a0b752494a46541a
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-11-18 11:24:16 +0200

    Add project files.

---
+++ Hangman/Gallows.cs
@@ -0,0 +1,36 @@
+﻿namespace HangmanMain
+{
+    public class Gallows
+    {
+        const int MaxChances = 6;
+        private int _chancesLeft;
+
+        public Gallows()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _chancesLeft = MaxChances;
+        }
+
+        public void ReduceChancesLeft()
+        {
+            _chancesLeft--;
+
+            if (_chancesLeft < 0)
+                _chancesLeft = 0;
+        }
+
+        public bool IsHung()
+        {
+            return _chancesLeft == 0;
+        }
+
+        public int GetChancesLeft()
+        {
+            return _chancesLeft;
+        }
+    }
+}
